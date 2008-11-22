aa97e1e939837130e3951283fbfe7d357209eb01
lenoxys <lenoxys@38686702-15cf-42e4-a595-3071df8bf5ea>
2008-11-22 22:17:47 +0000

    grammar fixes (thx momo61)

--- language/english.php
+++ language/english.php
@@ -22,7 +22,7 @@ $vm['_new_account_text2'] = 'You will need an account to play on Lineage II. If 
 			All fields marked with a <img src="./template/images/required.gif" width="14" height="14" alt="" /> are required fields and need to be completed in order to fill out the signup form successfully.
 ';
 $vm['_chg_pwd'] = 'Change your password';
-$vm['_chg_pwd_text'] = '<span style="color: red">Don\'t trust who say its GM/admin. WE  DON\'T NEED YOUR <u>ACCOUNT NAME</u> AND/OR <u>PASSWORD</u>! NEVER PROVIDE THEM!</span>';
+$vm['_chg_pwd_text'] = '<span style="color: red">Don\'t trust anyone claiming to be a GM/admin. WE DON\'T NEED YOUR <u>ACCOUNT NAME</u> AND/OR <u>PASSWORD</u>! NEVER PROVIDE THEM!</span>';
 $vm['_chg_button'] = 'Change';
 $vm['_password2'] = 'Confirm password';
 $vm['_passwordold'] = 'Old password';
@@ -46,13 +46,13 @@ $vm['_image_control_desc'] = 'To prevent automated registrations creation accoun
 If you can\'t read the image control, click on it for reload. ';
 $vm['_account_created'] = 'Your account has been created.';
 $vm['_account_actived'] = 'Your account has been actived.';
-$vm['_REGWARN_UNAME1'] = 'Please enter an username.';
-$vm['_REGWARN_UNAME2'] = 'Please enter a valid username.';
+$vm['_REGWARN_UNAME1'] = 'Please enter a username.';
+$vm['_REGWARN_UNAME2'] = 'Please enter an valid username.';
 $vm['_REGWARN_MAIL'] = 'Please enter a valid e-mail address.';
 $vm['_REGWARN_PASS'] = 'Please enter a valid password.  No spaces, more than 6 characters and contain 0-9,a-z,A-Z';
 $vm['_REGWARN_VPASS1'] = 'Please verify the password.';
 $vm['_REGWARN_VPASS2'] = 'Password and verification do not match, please try again.';
-$vm['_REGWARN_INUSE'] = 'This username is already in use. Please try another.';
+$vm['_REGWARN_INUSE'] = 'This username is already in use. Please try a different one.';
 $vm['_REGWARN_EMAIL_INUSE'] = 'This e-mail is already registered. If you forgot the password click on "Lost your Password" and a new password will be sent to you.';
 $vm['_WARN_NOT_LOGGED'] = 'You aren\'t logged';
 $vm['_REGWARN_LIMIT_CREATING'] = 'Quota of created account is full. Come back later';
@@ -64,9 +64,9 @@ $vm['_password_reseted'] = 'Password Reseted';
 $vm['_control'] = 'Control key isn\'t correct';
 
 $vm['_email_title_verif']			= '[SERVER] Address Verification';
-$vm['_email_message_verif']			= "Your email verification code is : [CODE].<br><br>You can completed registration by cliking <a href=\"[URL]\">here</a>.<br><br>If you have any questions about the registration process, please contact [EMAIL_SUPPORT].";
+$vm['_email_message_verif']			= "Your email verification code is : [CODE].<br><br>You can complete the registration by clicking <a href=\"[URL]\">here</a>.<br><br>If you have any questions about the registration process, please contact [EMAIL_SUPPORT].";
 $vm['_email_title_ok']				= 'Welcome To [SERVER]!';
-$vm['_email_message_ok']			= "Thank you for registering with [SERVER]!<br><br>Your [SERVER] account name is: [ID]<br><br>Lastly, if you have a question and just can't find the answer, our customer support team is always available as near as your email box.<br><br>Once again, thank you for joining the world of [SERVER]. We look forward to seeing you in-game!<br><br>The [SERVER] Team";
+$vm['_email_message_ok']			= "Thank you for registering on [SERVER]!<br><br>Your [SERVER] account name is: [ID]<br><br>Lastly, if you have a question and just can't find an answer, our customer support team is always available as near as your email box.<br><br>Once again, thank you for joining the world of [SERVER]. We look forward to seeing you in-game!<br><br>The [SERVER] Team";
 $vm['_email_title_change_pwd']		= 'Password Reset Request';
 $vm['_email_message_change_pwd']	= "Someone at [IP] want reset your Lineage II Game Account password for account [ID].<br>Click <a href=\"[URL]\">here</a> for reset your passord.<br>If you did not make this change, please don't care this mail'.<br><br>The [SERVER] Team";
 $vm['_email_title_change_pwd_ok']	= 'Password Reset Success';
