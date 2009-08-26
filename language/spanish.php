c896abb4fb51dd2d97fef4954f89f33bc40e8364
lenoxys <lenoxys@38686702-15cf-42e4-a595-3071df8bf5ea>
2009-08-26 11:48:14 +0000

    Update spanish language, thanks SHAKA

--- language/spanish.php
+++ language/spanish.php
@@ -8,95 +8,74 @@ $vm['_title'] = 'Helios - Asistente de Cuentas';
 $vm['_title_page'] = 'Asistente de Cuentas';
 $vm['_exist_account'] = 'Cuenta Existente';
 $vm['_account'] = 'Cuenta';
-$vm['_password'] = 'Contraseña';
+$vm['_password'] = 'Contrase&#241;a';
 $vm['_login_button'] = 'Entrar';
 $vm['_return'] = 'Atras';
 $vm['_new_account'] = 'Nueva Cuenta';
 $vm['_new_account_text'] = 'Para jugar a Lineage II, servidor privado, usted debe crear una cuenta.';
-$vm['_new_account_text2'] = 'Usted necesitará una cuenta para jugar Lineage II. Si usted ya tiene una cuenta, por favor identifique se.
-
-            Su cuenta no es transferible, y por tanto su nombre de cuenta como la contraseña deben ser confidenciales y no puede ser prestadas o dadas nadie!
-
-            <span style="color: red">No confíe en el GM/Admin, que pida su cuenta. No necesitamos la cuenta <u>Nombre de cuenta</u> o  su <u>Contraseña</u>! ¡Nunca la revele o suministre!</span>
-
-            Todos los campos con un <em class="me"></em> Deben ser rellenados obligatoriamente para terminar el registro con éxito.
-';
-$vm['_chg_pwd'] = 'Cambie su Contraseña';
-$vm['_chg_pwd_text'] = '<span style="color: red">¡No confíe GM/Admin que pida su cuenta. No necesitamos la cuenta <u>Nombre de cuenta</u> o <u>Contraseña</u>! Nunca la revele o suministre!</span>';
+$vm['_new_account_text2'] = 'Usted necesitar&#225; una cuenta para jugar Lineage II.<br />Si usted ya tiene una cuenta, por favor identifiquese.<br /><br />&#161;Su cuenta no es transferible, y por tanto su nombre de cuenta como la contrase&#241;a deben ser confidenciales y no puede ser prestadas o dadas nadie&#33;<br /><br /><span style="color: red">&#161;No conf&#237;e en el GM/Admin, que pida su cuenta&#33;<br />&#161;No necesitamos la cuenta <u>Nombre de cuenta</u> o  su <u>Contrase&#241;a</u>&#33; &#161;Nunca la revele o suministre&#33;</span><br /><br />Todos los campos con un <em class="me"></em> Deben ser rellenados obligatoriamente para terminar el registro con &#233;xito.';
+$vm['_chg_pwd'] = 'Cambie su Contrase&#241;a';
+$vm['_chg_pwd_text'] = '<span style="color: red">&#161;No conf&#237;e GM/Admin que pida su cuenta&#33;<br />&#161;No necesitamos la cuenta <u>Nombre de cuenta</u> o <u>Contrase&#241;a</u>&#33; &#161;Nunca la revele o suministre&#33;</span>';
 $vm['_chg_button'] = 'Cambiar';
 $vm['_password2'] = 'Confirmar';
 $vm['_passwordold'] = 'Actual';
-$vm['_email'] = 'Correo electrónico';
+$vm['_email'] = 'Correo electr&#243;nico';
 $vm['_create_button'] = 'Crear';
-$vm['_forgot_password'] = '¿Olvide mi Contraseña?';
-$vm['_forgot_pwd'] = 'Perdida de Contraseña';
-$vm['_forgot_pwd_text'] = 'Por favor verifique la información para actualizar su contraseña.
-
-            Si usted no logra recordar qué información puso durante el registro, usted necesitará contactar con el soporte.';
+$vm['_forgot_password'] = '&#191;Olvide mi Contrase&#241;a&#63;';
+$vm['_forgot_pwd'] = 'Perdida de Contrase&#241;a';
+$vm['_forgot_pwd_text'] = 'Por favor verifique la informaci&#243;n para actualizar su contrase&#241;a.<br />Si usted no logra recordar qu&#233; informaci&#243;n puso durante el registro, usted necesitar&#225; contactar con el soporte.';
 $vm['_forgot_button'] = 'Enviar';
 $vm['_logout'] = 'Usted se a desconectado.';
 $vm['_logout_link'] = 'Salir';
-$vm['_wrong_auth'] = 'Es invalido, por favor verifique su registro de entrada, Su información de registro de entrada fue ingresada incorrectamente, o la cuenta el sistema esta actualmente no disponible.';
-$vm['_no_id_no_pwd'] = 'Por favor ingrese un nombre de cuenta y la contraseña.';
-$vm['_email_registered'] = 'Este correo electrónico ya esta registrado en nuestra base de datos.';
+$vm['_wrong_auth'] = 'Es invalido, por favor verifique su registro de entrada, Su informaci&#243;n de registro de entrada fue ingresada incorrectamente, o la cuenta el sistema esta actualmente no disponible.';
+$vm['_no_id_no_pwd'] = 'Por favor ingrese un nombre de cuenta y la contrase&#241;a.';
+$vm['_email_registered'] = 'Este correo electr&#243;nico ya esta registrado en nuestra base de datos.';
 $vm['_image_control'] = 'El control de imagen no es correcto';
-$vm['_pwd_difference'] = 'Por favor repetir su contraseña';
-$vm['_pwd_incorrect'] = 'Por favor use otra contraseña';
-$vm['_image_control_desc'] = 'Para prevenir los registros automáticos la cuenta de creación exige que usted ingrese una clave de confirmación.
-Si usted no puede leer el control de imagen, haga clic en él, para que se cargue.';
+$vm['_pwd_difference'] = 'Por favor repetir su contrase&#241;a';
+$vm['_pwd_incorrect'] = 'Por favor use otra contrase&#241;a';
+$vm['_image_control_desc'] = 'Para prevenir los registros autom&#225;ticos la cuenta de creaci&#243;n exige que usted ingrese una clave de confirmaci&#243;n.<br /><br />Si usted no puede leer el control de imagen, haga clic en &#233;l, para que se cargue.';
 $vm['_account_created'] = 'Su cuenta ha sido creada.';
 $vm['_account_actived'] = 'Su cuenta a sido activada.';
 $vm['_REGWARN_UNAME1'] = 'Por favor ingrese un nombre de usuario.';
 $vm['_REGWARN_UNAME2'] = 'Por favor ingrese un nombre de usuario valido.';
-$vm['_REGWARN_MAIL'] = 'Por favor, introduzca una dirección correo electrónico valido.';
-$vm['_REGWARN_PASS'] = 'Por favor, introducir una contraseña valida. Sin espacios, Más que 6 caracteres. Puede contener: 0-9,a-z,A-Z';
-$vm['_REGWARN_VPASS1'] = 'Por favor, verifique la contraseña.';
-$vm['_REGWARN_VPASS2'] = 'La contraseña y la verificación no coinciden, por favor insertarla de nuevo.';
+$vm['_REGWARN_MAIL'] = 'Por favor, introduzca una direcci&#243;n correo electr&#243;nico valido.';
+$vm['_REGWARN_PASS'] = 'Por favor, introducir una contrase&#241;a valida. Sin espacios, m&#225;s de 6 caracteres. Puede contener: 0-9,a-z,A-Z';
+$vm['_REGWARN_VPASS1'] = 'Por favor, verifique la contrase&#241;a.';
+$vm['_REGWARN_VPASS2'] = 'La contrase&#241;a y la verificaci&#243;n no coinciden, por favor insertarla de nuevo.';
 $vm['_REGWARN_INUSE'] = 'Este nombre de usuario esta ya en uso. Por favor pruebe otro.';
-$vm['_REGWARN_EMAIL_INUSE'] = 'el correo electrónico ya esta registrado. Si usted olvida la contraseña, haga clic en "¿Olvide mi Contraseña?" y una nueva contraseña será enviada a usted.';
+$vm['_REGWARN_EMAIL_INUSE'] = 'el correo electr&#243;nico ya esta registrado. Si usted olvida la contrase&#241;a, haga clic en "&#191;Olvide mi Contrase&#241;a&#63;" y una nueva contrase&#241;a ser&#225; enviada a usted.';
 $vm['_WARN_NOT_LOGGED'] = 'Usted no esta registrado';
-$vm['_REGWARN_LIMIT_CREATING'] = 'El cupo del creador de cuentas está lleno. Vuelva después o consulto a un GM/Admin';
-
-$vm['_password_request'] = 'Envío de petición de contraseña.';
-$vm['_activation_control'] = 'La clave de activación no es correcta';
-
-$vm['_password_reseted'] = 'Contraseña reiniciada';
-$vm['_control'] = 'La clave de activación no es correcta';
-
-$vm['_email_title_verif']            = 'Verifique la dirección: [SERVER]';
-$vm['_email_message_verif']            = "Su clave de verificación de correo electrónico es: [CODE].<br /><br />Usted puede terminar el registro con clic <a href=\"[URL]\">Aquí</a>.<br /><br />Si usted tiene cualquier pregunta sobre el proceso de registro, Por favor contactar a [EMAIL_SUPPORT].";
-$vm['_email_title_ok']                = 'Bienvenido a [SERVER]!';
-$vm['_email_message_ok']            = "Gracias por registrarse en [SERVER]!<br /><br />Su nombre de cuenta en [SERVER] es: [ID]<br /><br />Para terminar, si usted tiene una pregunta y no puede encontrar la respuesta, nuestro equipo le ayudara a usted, está siempre disponible en su correo electrónico.<br /><br />Otra vez, Gracias a usted y Bienvenido a el mundo de [SERVER]. ¡Esperamos su visita en el juego!<br /><br />El Equipo de [SERVER]";
-$vm['_email_title_change_pwd']        = 'El reinicio de la petición de contraseña';
-$vm['_email_message_change_pwd']    = "La dirección IP [IP] quieren restablecer la contraseña de su cuenta de Lineage II a [ID].<br />haga clic <a href=\"[URL]\">aqui</a> para restablecer su contraseña.<br />Si no ha realizado este cambio, por favor, no tienen que preocuparse de este correo electrónico.<br /><br />El equipo de [SERVER]";
-$vm['_email_title_change_pwd_ok']    = 'El reinicio de su contraseña se cambiado correctamente';
-$vm['_email_message_change_pwd_ok']    = "La dirección IP [IP] ha restablecido su contraseña de su cuenta de el juego de Lineage II a [ID].<br /> Nueva contraseña: [CODE]<br /> Si usted no hiciera este cambio, por favor contacte con el soporte inmediatamente en [EMAIL_SUPPORT].";
-
-$vm['_email_title_change_email_ok']    = 'Tu correo electrónico a cambiado correctamente.';
-$vm['_email_message_change_email_ok']    = "La dirección IP [IP] a cambiado tu cuenta de Lineage II, el correo electrónico [ID].<br /> nuevo Correo electrónico : [CODE]<br /> Si usted no hiciera este cambio, por favor contacte con el soporte inmediatamente en [EMAIL_SUPPORT].";
-
-$vm['_creating_acc_prob'] = 'Problema en la base de datos : La cuenta no ha sido creada. Por favor, informe de esto al equipo de [SERVER] al correo electrónico [EMAIL_SUPPORT].';
-
-$vm['_chg_email']            = 'Cambiar su dirección de correo electrónico';
-$vm['_chg_email_text']        = '<span style="color: red">No confíe en quién dice ser GM/Admin. No necesitamos su <u>Nombre de cuenta</u> o <u>Contraseña</u>! Nunca la revele o suministre!</span>';
-$vm['_email2']                = 'Confirmar correo electrónico';
-$vm['_change_pwd_valid']    = 'Su contraseña se a cambiado correctamente.';
-$vm['_change_email_valid']    = 'Tu correo electrónico se a cambiado correctamente.';
-$vm['_REGWARN_VEMAIL1']        = 'El correo electrónico y la verificación no coinciden, por favor, intentarlo de nuevo.';
-
-$vm['_TERMS_AND_CONDITION']    = "<h2>Términos y condiciones</h2><br /><br />1. El Game Master siempre tiene la razón.<br />2. Si el Game Master es incorrecto referirse a la primera regla.";
+$vm['_REGWARN_LIMIT_CREATING'] = 'El cupo del creador de cuentas est&#225; lleno. Vuelva despu&#233;s o consulte a un GM/Admin';
+$vm['_password_request'] = 'Env&#237;o de petici&#243;n de contrase&#241;a.';
+$vm['_activation_control'] = 'La clave de activaci&#243;n no es correcta';
+$vm['_password_reseted'] = 'Contrase&#241;a reiniciada';
+$vm['_control'] = 'La clave de activaci&#243;n no es correcta';
+$vm['_email_title_verif'] = 'Verifique la direcci&#243;n: [SERVER]';
+$vm['_email_message_verif'] = "Su clave de verificaci&#243;n de correo electr&#243;nico es: [CODE].<br /><br />Usted puede terminar el registro, haciendo clic <a href=\"[URL]\">Aqu&#237;</a>.<br /><br />Si usted tiene cualquier pregunta sobre el proceso de registro, Por favor contactar a [EMAIL_SUPPORT].";
+$vm['_email_title_ok'] = 'Bienvenido a [SERVER]&#33;';
+$vm['_email_message_ok'] = "Gracias por registrarse en [SERVER]&#33;<br /><br />Su nombre de cuenta en [SERVER] es: [ID]<br /><br />Para terminar, si usted tiene una pregunta y no puede encontrar la respuesta, nuestro equipo le ayudara a usted, est&#225; siempre disponible en su correo electr&#243;nico.<br /><br />Otra vez, Gracias a usted y Bienvenido a el mundo de [SERVER]. &#161;Esperamos su visita en el juego&#33;<br /><br />El Equipo de [SERVER]";
+$vm['_email_title_change_pwd'] = 'El reinicio de la petici&#243;n de contrase&#241;a';
+$vm['_email_message_change_pwd'] = "La direcci&#243;n IP [IP] quieren restablecer la contrase&#241;a de su cuenta de Lineage II a [ID].<br />hacer clic <a href=\"[URL]\">aqu&#237;</a> para restablecer su contrase&#241;a.<br />Si no ha realizado este cambio, por favor, no tienen que preocuparse de este correo electr&#243;nico.<br /><br />El equipo de [SERVER]";
+$vm['_email_title_change_pwd_ok'] = 'El reinicio de su contrase&#241;a se cambiado correctamente';
+$vm['_email_message_change_pwd_ok'] = "La direcci&#243;n IP [IP] ha restablecido su contrase&#241;a de su cuenta de el juego de Lineage II a [ID].<br /> Nueva contrase&#241;a: [CODE]<br /> Si usted no a hecho este cambio, por favor contacte con el soporte inmediatamente en [EMAIL_SUPPORT].";
+$vm['_email_title_change_email_ok'] = 'Tu correo electr&#243;nico a cambiado correctamente.';
+$vm['_email_message_change_email_ok'] = "La direcci&#243;n IP [IP] a cambiado tu cuenta de Lineage II, el correo electr&#243;nico [ID].<br /> nuevo Correo electr&#243;nico : [CODE]<br /> Si usted no a hecho este cambio, por favor contacte con el soporte inmediatamente en [EMAIL_SUPPORT].";
+$vm['_creating_acc_prob'] = 'Problema en la base de datos: La cuenta no ha sido creada. Por favor, informe de esto al equipo de [SERVER] al correo electr&#243;nico [EMAIL_SUPPORT].';
+$vm['_chg_email'] = 'Cambiar su direcci&#243;n de correo electr&#243;nico';
+$vm['_chg_email_text'] = '<span style="color: red">&#161;No conf&#237;e en qui&#233;n dice ser GM/Admin&#33;<br />&#161;No necesitamos su <u>Nombre de cuenta</u> o <u>Contrase&#241;a</u>&#33; &#161;Nunca la revele o suministre&#33;</span>';
+$vm['_email2'] = 'Confirmar correo electr&#243;nico';
+$vm['_change_pwd_valid'] = 'Su contrase&#241;a se a cambiado correctamente.';
+$vm['_change_email_valid'] = 'Tu correo electr&#243;nico se a cambiado correctamente.';
+$vm['_REGWARN_VEMAIL1'] = 'El correo electr&#243;nico y la verificaci&#243;n no coinciden, por favor, intentarlo de nuevo.';
+$vm['_TERMS_AND_CONDITION'] = "<h2>T&#233;rminos y condiciones</h2><br /><br />1. El Game Master siempre tiene la raz&#243;n.<br />2. Si el Game Master es incorrecto referirse a la primera regla.";
 $vm['_accept_button'] = 'Aceptar';
-
-$vm['_select_worlds']            = 'Seleccione su mundo Lineage 2';
-$vm['_select_character']        = 'Seleccione su personaje';
-$vm['_character_fix']            = 'Arreglar su personaje';
-$vm['_character_unstuck']        = 'Mover su personaje a la ciudad más cercana.';
-$vm['_character_unstuck_yes']    = 'Su personaje se ha trasladado a la ciudad más cercana.';
-$vm['_character_unstuck_no']    = 'Su personaje no ha sido trasladado a la ciudad más cercana.';
-$vm['_character_fix_yes']        = 'Su personaje se ha arreglado.';
-$vm['_character_fix_no']        = 'Tu personaje no se ha arreglado.';
-
-$vm['_REGWARN_UNAME3'] = 'Por favor, elija un nombre de usuario diferente de su contraseña.';
-
-
+$vm['_select_worlds'] = 'Seleccione su mundo Lineage II';
+$vm['_select_character'] = 'Seleccione su personaje';
+$vm['_character_fix'] = 'Arreglar su personaje';
+$vm['_character_unstuck'] = 'Mover su personaje a la ciudad m&#225;s cercana.';
+$vm['_character_unstuck_yes'] = 'Su personaje se ha trasladado a la ciudad m&#225;s cercana.';
+$vm['_character_unstuck_no'] = 'Su personaje no ha sido trasladado a la ciudad m&#225;s cercana.';
+$vm['_character_fix_yes'] = 'Su personaje se ha arreglado.';
+$vm['_character_fix_no'] = 'Tu personaje no se ha arreglado.';
+$vm['_REGWARN_UNAME3'] = 'Por favor, elija un nombre de usuario diferente de su contrase&#241;a.';
 ?>
